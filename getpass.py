66633c489c198589e98159cf790ba111f0a9d1e9
tejaswinikommu-14 <kommutejaswini84@gmail.com>
2021-02-02 15:28:22 +0530

    Third commit

---
+++ getpass.py
@@ -0,0 +1,10 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> uname=input('enter u name:-')
+pwd=input('enter pwd:-')
+
+
+if uname=="teju" and pwd=="teju1234":
+     print('welcome',uname)
+else:
+    print('invalid uname and password')
---
+++ string.py
@@ -0,0 +1,76 @@
+# string
+"""---> colection of  characters  is called string
+----->group of charactres is called string
+----->in python string representation is ' 'or " "or " " "
+----->in python strings are immutable
+------>in python string is indexed  value based
+------>string supports slicing operator(':')
+"""
+a=" "
+#print(type(a))
+#a="welcome to python"
+#print(a)
+
+# concatenation
+#b='workshop'
+#print(a+b)
+
+# getting values
+#print(a[0])
+#print(a[1])
+# forward index 
+#a[0]
+#a[5]
+# backword index
+#print(a[-1])
+#print(a[-5])
+# slicing opration--->:
+name='python programing'
+#print(name[:])
+# reversing astring
+#print(name[::-1])
+#print(name[0:17:2])
+#print(name[0:])
+
+# sr=tring  Methods 3 ways
+# bu using datatype--->str
+#print(dir(str))
+#n=''
+#print(dir(n))
+#m='hello'
+#print(dir(m))
+p='welcome to python'
+#print(p.capitalize())---> prints the uppercase character
+#print(p.casefold())
+#print(p.swapcase())
+#print(p.count('S'))
+#print(p.index('p'))
+#print(p.index('o'))
+#print(p.index('e'))
+#print(p.find('x'))
+#print(p.center(20,'*'))
+#print(len(p))
+#print('My name is {0} & i am from{1}'.format('abc','xyz'))
+#print(p.upper())
+#print(p.lower())
+#print(p.title())
+#print(p.isupper())
+#print(p.islower())
+#print(p.startswith('W'))
+#print(p.endswith(' c'))
+#print(p.istitle())
+#q='<---'
+#print(q.join(p))
+#print(p.isdigit())
+#print(p.isalpha())
+#print(p.isalnum())
+#print('python\tprogramming\tworkshop'.expandtabs())
+''' split,
+splitlines,
+strip,
+istrip,
+rstrip'''
+#print(p.split())
+#print(p.strip())
+#print(p.lstrip())
+print(p.rstrip())
